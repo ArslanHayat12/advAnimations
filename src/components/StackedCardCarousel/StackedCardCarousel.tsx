347cc4e5d4c75871ddfab65c91e55a204c612445
Muhammad Talha <muhammadtalha04@gmail.com>
2021-05-27 13:02:02 +0500

    Moved stack cards logic to utils

--- src/components/StackedCardCarousel/StackedCardCarousel.tsx
+++ src/components/StackedCardCarousel/StackedCardCarousel.tsx
@@ -6,6 +6,7 @@ import React, {
   useRef,
   useState,
 } from 'react';
+import { stackCards } from '../../utils';
 import Card from '../Card/Card';
 import { StackStyle } from './Style';
 
@@ -23,7 +24,7 @@ const StackedCardCarousel: FC<StackedCardCarouselProps> = ({
   const cardRefs = useRef<Array<HTMLDivElement | null>>([]);
   const [cardAtTop, setCardAtTop] = useState<number>(0);
 
-  const stackCard = useCallback(
+  const handleCardClick = useCallback(
     (cardIndex: number) => {
       if (cardIndex !== cardAtTop) {
         // References of all the cards uptil the current card
@@ -34,44 +35,7 @@ const StackedCardCarousel: FC<StackedCardCarouselProps> = ({
           sliceEnd
         );
 
-        if (cardIndex > cardAtTop) {
-          for (let i: number = 0; i < cardIndex; i++) {
-            const card: HTMLDivElement | null = cards[i]; // Getting the current card
-
-            // Checking if the reference to the current card is not null
-            if (card !== null) {
-              const cardHeight: number = card.clientHeight - 15; // Getting current card's height to be used for stacking next card on it
-
-              // Scaling the current card to show a stack
-              const scale: number = 1 - (cardIndex - i) * 0.03;
-              card.style.transform = `scale(${scale})`;
-
-              // Stacking the next card on current card
-              cards[i + 1]!.style.marginTop = `-${cardHeight}px`;
-            }
-          }
-        } else {
-          // debugger;
-          for (let i = cardAtTop; i > cardIndex; i--) {
-            const card: HTMLDivElement | null = cards[i]; // Getting the current card
-
-            // Checking if the reference to the current card is not null
-            if (card !== null) {
-              card.style.transform = 'scale(1)';
-              card.style.margin = '10px 0px';
-            }
-          }
-
-          for (let i = cardIndex; i >= 0; i--) {
-            const card: HTMLDivElement | null = cards[i]; // Getting the current card
-
-            // Checking if the reference to the current card is not null
-            if (card !== null) {
-              const scale: number = 1 - (cardIndex - i) * 0.03;
-              card.style.transform = `scale(${scale})`;
-            }
-          }
-        }
+        stackCards(cardIndex, cardAtTop, cards);
 
         setCardAtTop(cardIndex);
       }
@@ -86,12 +50,12 @@ const StackedCardCarousel: FC<StackedCardCarouselProps> = ({
         index={index}
         reference={cardRefs}
         content={cardContent}
-        onCardClick={stackCard}
+        onCardClick={handleCardClick}
         cardbackground={cardsBackgroundColor}
         cardtextcolor={cardsContentColor}
       />
     ));
-  }, [cardsContent, cardsBackgroundColor, cardsContentColor, stackCard]);
+  }, [cardsContent, cardsBackgroundColor, cardsContentColor, handleCardClick]);
 
   return <StackStyle>{renderCards}</StackStyle>;
 };
