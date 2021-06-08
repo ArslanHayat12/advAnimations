e48e0f025165624a23429be31039838751a6fc82
Muhammad Talha <muhammadtalha04@gmail.com>
2021-06-08 09:18:47 +0500

    Fixed linting issues

--- src/components/ControlItem/ControlItem.tsx
+++ src/components/ControlItem/ControlItem.tsx
@@ -2,12 +2,12 @@ import React, { MouseEventHandler } from 'react';
 import { ControlItemStyle } from './Style';
 
 interface ControlItemProps {
-	selected: boolean;
-	onClick: MouseEventHandler<HTMLDivElement>;
+  selected: boolean;
+  onClick: MouseEventHandler<HTMLDivElement>;
 }
 
 const ControlItem: React.FC<ControlItemProps> = ({ selected, onClick }) => {
-	return <ControlItemStyle selected={selected} onClick={onClick} />;
+  return <ControlItemStyle selected={selected} onClick={onClick} />;
 };
 
 export default ControlItem;
--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { ImageStyle } from './Style';
 
 interface ImageProps {
-	imagePath: string;
+  imagePath: string;
 }
 
 const Image: React.FC<ImageProps> = ({ imagePath }) => {
-	return <ImageStyle imagepath={imagePath} />;
+  return <ImageStyle imagepath={imagePath} />;
 };
 
 export default Image;
