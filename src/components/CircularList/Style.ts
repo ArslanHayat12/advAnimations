dbeb3d4e6375c75c42209409826ddc3e6c52e16d
Muhammad Talha <muhammadtalha04@gmail.com>
2021-06-24 10:26:02 +0500

    Added option to display images in the list and option to display/hide the circle border

--- src/components/CircularList/Style.ts
+++ src/components/CircularList/Style.ts
@@ -9,7 +9,14 @@ interface ListItemProps {
 
 interface ItemDivProps extends ListItemProps {
   selected: boolean;
+  isimage: boolean;
+  background?: string;
+  color?: string;
 }
+
+type TextItemProps = {
+  overflow: boolean;
+};
 
 export const Container = styled('div')`
   display: flex;
@@ -21,14 +28,12 @@ export const Container = styled('div')`
 `;
 
 export const Circle = styled('div')`
+  position: relative;
   height: 100vmin;
   width: 100vmin;
   border-radius: 50%;
   margin-left: calc(-50vmin - 100px);
 
-  @media (max-width: 900px) {
-    margin-top: 100px;
-  }
   @media (max-width: 700px) {
     margin-top: 80px;
   }
@@ -68,7 +73,6 @@ export const ListItem = styled('div', forwardRef)<ListItemProps>`
 export const Bar = styled('div')`
   position: relative;
   width: 35vmin;
-  height: 0px;
 
   @media (min-width: 820px) {
     width: 40vmin;
@@ -91,21 +95,112 @@ export const Bar = styled('div')`
 `;
 
 export const ItemDiv = styled('div')<ItemDivProps>`
-  color: black;
   cursor: pointer;
+  border-radius: 50%;
   z-index: 2;
-  padding: 20px;
-  max-width: 30vmin;
-  overflow: hidden;
-  transition: transform 0s;
+  transition: transform 0.5s;
   transform-origin: 15px 0px;
-  ${({ selected, angle }) => {
+  ${({ selected, angle, isimage, background, color }) => {
     return `
-		transform: ${
-      selected === true ? 'scale(1.1)' : 'scale(1)'
-    } rotate(calc(360deg - ${angle}deg));
-		opacity: ${selected === true ? 1 : 0.7};
-		display: ${(Math.abs(angle) === 90) === true ? 'none' : 'block'};
+      background: ${
+        isimage
+          ? 'transparent'
+          : background !== undefined
+          ? background
+          : '#000000'
+      };
+      color: ${color !== undefined ? color : '#ffffff'};
+      transform: ${
+        selected === true ? 'scale(1.15)' : 'scale(1)'
+      } rotate(calc(360deg - ${angle}deg));
+      opacity: ${selected === true ? 1 : 0.7};
+      display: ${(Math.abs(angle) === 90) === true ? 'none' : 'block'};
 		`;
   }}
+`;
+
+export const ImageItem = styled('img')`
+  width: 100px;
+  height: 100px;
+  margin: 20px;
+
+  @media (max-width: 700px) {
+    width: 80px;
+    height: 80px;
+    margin: 15px;
+  }
+  @media (max-width: 650px) {
+    width: 75px;
+    height: 75px;
+    margin: 10px;
+  }
+  @media (max-width: 560px) {
+    width: 65px;
+    height: 65px;
+    margin: 8px;
+  }
+`;
+
+export const TextItem = styled('p')<TextItemProps>`
+  display: flex;
+  width: 110px;
+  height: 110px;
+  margin: 15px;
+  align-items: center;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  ${({ overflow }) => `
+    justify-content: ${overflow === true ? 'left' : 'center'};
+  `}
+
+  @media (max-width: 700px) {
+    width: 90px;
+    height: 90px;
+    margin: 10px;
+  }
+  @media (max-width: 650px) {
+    width: 75px;
+    height: 75px;
+    margin: 10px;
+  }
+  @media (max-width: 560px) {
+    width: 65px;
+    height: 65px;
+    margin: 8px;
+  }
+`;
+
+export const CircleBorderStyle = styled('div')`
+  position: absolute;
+  width: 80vmin;
+  height: 80vmin;
+  top: 10vmin;
+  left: -2vmin;
+  border: 2px solid gray;
+  border-radius: 50%;
+
+  @media (max-width: 820px) {
+    top: 10vmin;
+    left: 6vmin;
+  }
+  @media (max-width: 750px) {
+    top: 7vmin;
+    left: 2vmin;
+  }
+  @media (max-width: 650px) {
+    top: 5vmin;
+    left: 2vmin;
+  }
+  @media (max-width: 550px) {
+    top: -7vmin;
+    left: -2vmin;
+  }
+  @media (max-width: 450px) {
+    top: -18vmin;
+    left: 0vmin;
+  }
+  @media (max-width: 400px) {
+    top: -20vmin;
+    left: 0vmin;
+  }
 `;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,7 @@
 export type DivRefType = HTMLDivElement | null;
 export type AccordionSize = 'full' | 'auto';
+export type CircularListItem = {
+  id: number;
+  value: string;
+  path?: string;
+};
